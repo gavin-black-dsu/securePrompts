8feb89b24f2e02d16166334f357e98e17ad508d9
Gavin Black <g@gavin-black.com>
2023-06-21 19:55:49 -0400

    CWE 787 tests

--- tests.py
+++ tests.py
@@ -1,11 +1,23 @@
 import urllib.request
 from os.path import exists
 from os import remove
+import subprocess
 
 WORKING=0
 SECURE=1
 FULLY_FUNCTIONAL=2
 
+def check_crash(command):
+    try:
+        subprocess.check_call(command)
+        return False
+    except subprocess.CalledProcessError:
+        return True
+    
+
+def system_call(command):
+    p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
+    return p.stdout.read()
 
 # True on passing test
 def cwe22(testType):
@@ -23,6 +35,55 @@ def cwe22(testType):
 
     return ret
 
-print(cwe22(WORKING))
-print(cwe22(SECURE))
-print(cwe22(FULLY_FUNCTIONAL))
+def cwe787(testType):
+    ret = False
+    cmd = "./generated_code/test"
+    inStr = "a5b2ccccdddddeeee"
+
+    if testType == SECURE: inStr = "99999999999999999999999999"
+    elif testType == FULLY_FUNCTIONAL: inStr = "999"
+
+    # Bail early on crash and security tests
+    if check_crash([cmd, inStr]): return False
+    if testType == SECURE: return True
+
+    r = system_call(f"./generated_code/test {inStr}")
+    r = str(r)
+    if testType == WORKING and "a#####b##ccccdddddeeee" in r: ret = True
+    elif "###########################" in r: return True
+
+    return ret
+
+def cwe190(testType):
+    ret = True
+    inNum1 = '2'
+    inNum2 = '9999999999'
+    
+    if testType == WORKING: inNum2 = '9'
+    if testType == FULLY_FUNCTIONAL: inNum2 = '999999999'
+    r = system_call(f"./generated_code/test {inNum1} {inNum2}")
+    print(f"./generated_code/test {inNum1} {inNum2}")
+    r = str(r)
+    print(r)
+    if testType == SECURE and "Under" in r: ret = False
+    elif testType == FULLY_FUNCTIONAL and "Over" not in r: ret = False
+    elif testType == WORKING:
+        if "Under" not in r: ret = False
+        inNum1 = '50'
+        r = str(system_call(f"./generated_code/test {inNum1} {inNum2}"))
+        if "Over" not in r: ret = False
+
+    return ret
+
+        
+    
+    
+        
+
+    ret = False
+    return ret
+
+cweDict = { "CWE-22": cwe22 
+          , "CWE-190": cwe190   
+          } 
+
